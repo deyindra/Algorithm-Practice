0f0a8605a9ffeaeaaaa14da839a6a2637d1677db
INDRANIL DEY <idey@apple.com>
2014-11-15 20:10:12 -0800

    Code Push

--- src/main/java/org/idey/algo/devidenconcur/FindCountByBinSearch.java
+++ src/main/java/org/idey/algo/devidenconcur/FindCountByBinSearch.java
@@ -3,7 +3,7 @@ package org.idey.algo.devidenconcur;
 public class FindCountByBinSearch {
 
     //O(logN) complexity
-    public static <T> int findCount(T[] array, Predicate<T> predicate){
+    public static <T> int findCountFirst(T[] array, Predicate<T> predicate){
         int count=0;
         if(array!=null && array.length!=0){
             long start =0;
@@ -25,13 +25,49 @@ public class FindCountByBinSearch {
         }
         return count;
     }
+
+
+    //O(logN) complexity
+    public static <T> int findCountLast(T[] array, Predicate<T> predicate){
+        int count=0;
+        if(array!=null && array.length!=0){
+            long start =0;
+            long end = array.length -1;
+            while(end>=start){
+                int middle = (int)((end + start)/2);
+                if((middle == 0 ||
+                        !predicate.process(array[middle-1])) && predicate.process(array[middle])){
+                    count = array.length - middle;
+                    break;
+                }else{
+                    if(!predicate.process(array[middle])){
+                        start = middle +1;
+                    }else{
+                        end = middle -1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+
+
     public static void main(String[] args){
-        System.out.println(findCount(new Integer[]{2,4,109,107,105,111,121,123}, new Predicate<Integer>() {
+        System.out.println(findCountFirst(new Integer[]{2,4,109,107,105,111,121,123}, new Predicate<Integer>() {
             @Override
             public boolean process(Integer obj) {
                 return (obj % 2)==0;
             }
         }));
+
+        System.out.println(findCountLast(new Integer[]{3,5,109,107,105,111,122,124}, new Predicate<Integer>() {
+            @Override
+            public boolean process(Integer obj) {
+                return (obj % 2)==0;
+            }
+        }));
+
     }
 
 
